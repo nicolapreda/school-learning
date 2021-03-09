5b9cc6807f4ff82067ea88ae0ef3d500cc20f97c
diskxo <nicola.preda05@gmail.com>
2021-03-09 18:59:07 +0100

    🎉FEATURE: Created GuessTheNumber.go

---
+++ GuessNumber.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	risposta := "<"
+
+	valoreMinimo := 0
+	valoreMassimo := 100
+
+	numero := (valoreMassimo + valoreMinimo) / 2
+	fmt.Println("Pensa ad un numero compreso tra 1 e 100, proverò a indovinarlo.\n(I valori consentiti sono <, > o =)")
+	for risposta != "=" {
+		fmt.Println("Il numero che hai pensato è maggiore, minore o uguale a", int(numero), "?")
+		fmt.Scanln(&risposta)
+		if risposta == "<" {
+			valoreMassimo = numero
+			numero = (valoreMassimo + valoreMinimo) / 2
+		} else if risposta == ">" {
+			valoreMinimo = numero
+			numero = (valoreMassimo + valoreMinimo) / 2
+		}
+	}
+	fmt.Println("Il numero che hai pensato è:", int(numero), "! :D")
+
+}
