a767c79ce7e7cf6d3017a5fb9592736bcb5578d6
Nicola Preda <preda.nicola.studente@itispaleocapa.it>
2021-12-15 15:51:24 +0100

    Added night, functions organization

--- Paesaggio/main.cpp
+++ Paesaggio/main.cpp
@@ -7,75 +7,60 @@
 
 using namespace std;
 
+//Variabili colori
+int cielo_giorno = COLOR(138, 196, 255);
+int terreno = COLOR(54,36,35);
+int tronco_albero = COLOR(46, 39, 24);
+int sole_giorno = COLOR(245, 236, 73);
+int colore_casa = COLOR(182,33,33);
+int porta = COLOR(48,32,3);
+int porta_bordi = COLOR(68,46,4);
+int nuvola = COLOR(200,255,255);
+int tetto = COLOR(255,127,39);
+int cielo_notte = COLOR(20,41,97);
+int colore_luna = COLOR(245,185,3);
+
+int linea_orizzonte = rand() % 200 + 400;
 
 void riempi(int color1, int riempimento, int x, int y){
-		setfillstyle(0,riempimento);
+		setfillstyle(1,riempimento);
     	floodfill(x,y,color1);
 }
-	
-	
-int main(){
-	
-	
-	//Variabili colori
-	const int cielo_giorno = COLOR(138, 196, 255);
-	const int terreno = COLOR(54,36,35);
-	const int tronco_albero = COLOR(46, 39, 24);
-	const int sole_giorno = COLOR(245, 236, 73);
-	const int casa = COLOR(182,33,33);
-	const int porta = COLOR(48,32,3);
-	const int porta_bordi = COLOR(68,46,4);
-	const int nuvole = COLOR(200,255,255);
-	const int tetto = COLOR(255,127,39);
-	
-	//Inizializzo finestra grafica
-	initwindow(1600,900);
-	
-	srand (time(0));
-	
-	/*Set linea orizzonte*/	
-	int linea_orizzonte = rand() % 200 + 400;
-	
-	setcolor(terreno);
-	setlinestyle(0,0,4); // Spessore linea
-	
-	line(0,linea_orizzonte,getmaxx(),linea_orizzonte);
-	/*Set terra*/
-	setfillstyle(1,cielo_giorno);
-	floodfill(0, 0,terreno);
-	
-	/*Set cielo*/
-	setfillstyle(1,porta);
-	floodfill(getmaxx(), getmaxy(),terreno);
-	
+
+void sole(){
 	//Set Sole
 	int x_sole = rand() % 900 + 50;
 	int y_sole = getmaxy() - linea_orizzonte - 300;
 	setcolor(sole_giorno);
 	circle (x_sole, y_sole, 100);
 	
-	setfillstyle(1,sole_giorno);
-	floodfill(x_sole , y_sole +80,sole_giorno);
-	
-	//Set nuvole
-	setcolor(nuvole);
+	//Riempimento sole
+	riempi(sole_giorno, sole_giorno, x_sole, y_sole);
+
+}
+
+void nuvole(){
+	setcolor(nuvola);
 	int n_nuvole = rand() % 5 + 1;
 	for(int c = 0; c < n_nuvole; c++){
 		int xnuvola = rand() % 1500 + 50;
-		int ynuvola = rand() % (linea_orizzonte - 20);
+		int ynuvola = rand() % linea_orizzonte;
 		int stangle = 0, endangle = 360;
 		int xradius = rand() % 75 + 25, yradius = rand() % 30 + 20;
 		ellipse(xnuvola, xnuvola, stangle, endangle, xradius, yradius);
-		setfillstyle(1,nuvole);
-		floodfill(xnuvola , xnuvola,nuvole);
+		setfillstyle(1,nuvola);
+		floodfill(xnuvola , xnuvola,nuvola);
 		
 		ellipse(xnuvola +30, ynuvola + 50, stangle, endangle, xradius, yradius);
-		setfillstyle(1,nuvole);
-		floodfill(xnuvola +30 , ynuvola + 50,nuvole);
+		setfillstyle(1,nuvola);
+		floodfill(xnuvola +30 , ynuvola + 50,nuvola);
 	}
-			
+}
+
+
+void casa(){
 	//Casa
-	setcolor(casa);
+	setcolor(colore_casa);
 	int x_casa = rand() % 1500 + 50;
 	int h_casa = rand() % 250 + 120;
 	int l_casa = rand() % 500 + 200;
@@ -108,8 +93,8 @@ int main(){
     setfillstyle(1,tetto);
     floodfill(x_tetto, linea_orizzonte - h_casa - 10, tetto);
     
-    
-	//Erba
+}
+void erba(){
 	for(int c = 0; c < 100000; c++){
 		int x_erba = rand() % getmaxx();
 		int y_erba = rand() % getmaxy() + (linea_orizzonte - 5);
@@ -119,7 +104,9 @@ int main(){
 		//Set arco erba
 		arc(x_erba, y_erba, x_erba, y_erba + lunghezza_erba, 2);
 	}
-	
+}
+
+void albero(){
 	//Albero
 	setcolor(COLOR(108,54,0));
     int xtronco=rand()%getmaxx();
@@ -148,8 +135,96 @@ int main(){
         	floodfill(xfoglia,yfoglia,COLOR(0,193,49));
 		}
 	}
-		
+}
+
+void giorno(){
 	
+	setcolor(terreno);
+	setlinestyle(0,0,4); // Spessore linea
+	
+	line(0,linea_orizzonte,getmaxx(),linea_orizzonte);
+	//Colore cielo
+	riempi(terreno, cielo_giorno,0,0);
+	//Colore terreno
+	riempi(terreno, porta,getmaxx(),getmaxy());
+	
+	
+	//Imposta sole
+	sole();
+	//Imposta nuvole
+	nuvole();
+	//Imposta casa
+	casa();	
+	//Imposta erba
+	erba();
+	//Imposta albero
+	albero();
+}
+
+void stelle(){
+	
+}
+
+void luna(){
+	//Set Luna
+	int x_luna = rand() % 900 + 50;
+	int y_luna = getmaxy() - linea_orizzonte - 300;
+	setcolor(colore_luna);
+	circle (x_luna, y_luna, 100);
+	//Riempimento luna
+	riempi(colore_luna, colore_luna, x_luna, y_luna);
+	
+	setcolor(cielo_notte);
+	circle (x_luna - 100, y_luna, 100);
+	riempi(cielo_notte, cielo_notte, x_luna - 100, y_luna);
+}
+
+void notte(){
+	
+	setcolor(terreno);
+	setlinestyle(0,0,4); // Spessore linea
+	
+	line(0,linea_orizzonte,getmaxx(),linea_orizzonte);
+	//Colore cielo
+	riempi(terreno, cielo_notte,0,0);
+	//Colore terreno
+	riempi(terreno, porta,getmaxx(),getmaxy());
+	
+	
+	luna();
+	//Imposta casa
+	casa();	
+	//Imposta erba
+	erba();
+	//Imposta albero
+	albero();
+
+}
+	
+int main(){	
+
+	srand (time(0));
+	//Inizializzo finestra grafica
+	initwindow(1600,900);
+	short int time = rand() % 5 + 1;
+	switch(time){
+		case 1:
+			notte();
+			break;
+		case 2:
+			notte();
+			break;
+		case 3:
+			notte();
+			break;
+		case 4:
+			notte();
+			break;
+		case 5:
+			notte();
+			break;
+	}
+
 	getch();
 	return 0;
 }
