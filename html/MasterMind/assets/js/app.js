056e693db6b6028de831127788efdcf4639d8980
Nicola Preda <preda.nicola.studente@itispaleocapa.it>
2022-01-04 00:47:45 +0100

    Updated app.js

--- html/MasterMind/assets/js/app.js
+++ html/MasterMind/assets/js/app.js
@@ -1,48 +1,70 @@
-$(document).ready(function () {
-  
-  let checkBoxSequence = document.getElementById("nSequence")
-  checkBoxSequence.addEventListener("change", function () {
-    let nSequence = parseInt(this.value)
-    console.log(nSequence)
-  });
+$(document).ready(function() {
+    let checkBoxSequence = document.getElementById("nSequence");
+    checkBoxSequence.addEventListener("change", function() {
+        let nSequence = parseInt(this.value);
+        console.log(nSequence);
+    });
 
-  let checkBoxNColors = document.getElementById('nColors')
-  checkBoxNColors.addEventListener("change", function () {
-    let nColors = parseInt(this.value)
-    console.log(nColors)
-  });
-  
-  function CheckRepeatedColors() {
+    let checkBoxNColors = document.getElementById("nColors");
+    checkBoxNColors.addEventListener("change", function() {
+        let nColors = parseInt(this.value);
+        console.log(nColors);
+    });
+    let repeatedColors;
+
     // Get the checkbox
-    var isRepeatedColors = document.getElementById("RepeatedColors");
+    let isRepeatedColors = document.getElementById("RepeatedColors");
 
     if (isRepeatedColors.checked == true) {
+        repeatedColors = true;
     } else {
+        repeatedColors = false;
     }
-  }
 
-  //Array  with possible colors
-  let possibleColors = ["blue", "green", "red", "yellow", "orange", "pink"];
+    //Array  with possible colors
+    let possibleColors = ["blue", "green", "red", "yellow", "orange", "pink"];
 
-  let colors = {
-    "rgb(0, 128, 0)": "green",
-    "rgb(255, 255, 0)": "yellow",
-    "rgb(255, 0, 0)": "red",
-    "rgb(0, 0, 255)": "blue",
-    "rgb(255, 192, 203)": "pink",
-    "rgb(255, 165, 0)": "orange",
-  };
+    let colors = {
+        "rgb(0, 128, 0)": "green",
+        "rgb(255, 255, 0)": "yellow",
+        "rgb(255, 0, 0)": "red",
+        "rgb(0, 0, 255)": "blue",
+        "rgb(255, 192, 203)": "pink",
+        "rgb(255, 165, 0)": "orange",
+    };
 
-  let hasWon = false;
+    let hasWon = false;
 
-  let code = [
-    possibleColors[Math.floor(Math.random() * 6)],
-    possibleColors[Math.floor(Math.random() * 6)],
-    possibleColors[Math.floor(Math.random() * 6)],
-    possibleColors[Math.floor(Math.random() * 6)],
-  ];
+    //Define start button
+    let startBtn = document.getElementById("start");
+    //When start button is clicked
+    $(startBtn).click(function() {
+        //Disable options
+        checkBoxSequence.setAttribute("disabled", "");
+        checkBoxNColors.setAttribute("disabled", "");
+        isRepeatedColors.setAttribute("disabled", "");
 
-  let startBtn = document.getElementById("start");
+        //Set sequence code
+        let code = [];
+        for (let i = 0; i < nSequence; i++) {
+            code[i] = possibleColors[Math.floor(Math.random() * 6)];
+        }
+        console.log(code);
 
-  
+        //change the current color when the user clicks on the color board
+        $(".color").click(function() {
+            let color = $(this).attr("id");
+            currentColor = color;
+            $(".currentColor").css("background-color", color);
+        });
+
+        //change the color of a board cell on click
+        $(".boardCell").click(function() {
+            var id = $(this).attr("id");
+
+            if (isValid(id)) {
+                $(this).css("background-color", currentColor);
+            }
+        });
+    });
 });
