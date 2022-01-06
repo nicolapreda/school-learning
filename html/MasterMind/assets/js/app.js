01d31048d34a247924063cffd03b16ffec1dc711
Nicola Preda <preda.nicola.studente@itispaleocapa.it>
2022-01-06 21:10:12 +0100

    Updated app.js & index.html

--- html/MasterMind/assets/js/app.js
+++ html/MasterMind/assets/js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   checkBoxSequence.addEventListener("change", function () {
     let nSequence = parseInt(this.value);
     console.log(nSequence);
-    
+
   });
 
   let checkBoxNColors = document.getElementById("nColors");
@@ -25,55 +25,70 @@ $(document).ready(function () {
     repeatedColors = false;
   }
 
-  let colors = {
-    "rgb(0, 128, 0)": "green",
-    "rgb(255, 255, 0)": "yellow",
-    "rgb(255, 0, 0)": "red",
-    "rgb(0, 0, 255)": "blue",
-    "rgb(255, 192, 203)": "pink",
-    "rgb(255, 165, 0)": "orange",
-  };
 
-  let hasWon = false;
 
   //Define start button
   let startBtn = document.getElementById("start");
   //When start button is clicked
   $(startBtn).click(function () {
+    let hasWon = false;
+
+
     //Disable options
     checkBoxSequence.setAttribute("disabled", "");
     checkBoxNColors.setAttribute("disabled", "");
     isRepeatedColors.setAttribute("disabled", "");
     startBtn.setAttribute("disabled", "");
     typeMode.setAttribute("disabled", "");
-    possibleColors.slice(0, -nColors )
+    possibleColors.slice(0, -nColors)
     console.log(possibleColors)
 
-    //Set the colors
-    for(var c = 1; c <= parseInt(nColors.value); )
 
-    //Set the game visible
-    let gameContent = document.getElementById('gameContainer');
-    gameContent.style.display = 'initial';
-    
+
+
+    //let gameContent = document.getElementById('gameContainer');
+    //gameContent.style.display = 'initial';
+
+    //Set the colors
+
+    let colors = ["rgb(0, 0, 255)", // Blue
+      "rgb(0, 128, 0)",//Green
+      "rgb(255, 0, 0)",//Red
+      "rgb(255, 255, 0)", //Yellow
+      "rgb(255, 165, 0)", //Orange
+      "rgb(255, 192, 203)", //Pink
+      "rgb(255, 255, 255)", // White
+      "rgb(0,0,0)", // Black
+      "rgb(79, 44, 27)", // Brown
+      "rgb(78, 191, 50)"] //Light Green
+
+    //Create color backgrounds
+    for (var c = 0; c < parseInt(nColors.value); c++) {
+      if (c < 5) {
+        document.getElementById('colorsContent1').insertAdjacentHTML('beforeend', '<div class="square square-lg m-2"><button class="w-h-100 color" style="background-color:' + colors[c] + '" id="' + colors[c] + '"></div>');
+      }
+      else {
+        document.getElementById('colorsContent2').insertAdjacentHTML('beforeend', '<div class="square square-lg m-2"><button class="w-h-100 color" style="background-color:' + colors[c] + '" id="' + colors[c] + '"></div>');
+      }
+    };
+
     //Set sequence code
     let code = [];
-    for (let i = 0; i < nSequence; i++) {
+    for (let i = 0; i < parseInt(nSequence.value); i++) {
       code[i] = possibleColors[Math.floor(Math.random() * 6)];
     }
     console.log(code);
 
     //Insert sockets into the interface
-    for(var i = 1; i <= 10; i++){
-      for(var s = 1; s <= parseInt(nSequence.value); s++){
-        
-        document.getElementById('slot' + i).insertAdjacentHTML('afterbegin','<div class="square square-lg m-2"><button class="w-h-100 socket" id="socket' + s + '"></div>' );
+    for (var i = 1; i <= 10; i++) {
+      for (var s = 1; s <= parseInt(nSequence.value); s++) {
+
+        document.getElementById('slot' + i).insertAdjacentHTML('afterbegin', '<div class="square square-lg m-2"><button class="w-h-100 socket" id="socket' + s + '"></div>');
       }
       //Insert Socket results
       document.getElementById('slot' + i).insertAdjacentHTML('beforeend', '<div class="roundresult"><div class="d-flex"><div class="square square-lg mr-1"><button class="w-h-100 socketresult" disabled></div><div class="square square-lg"><button class="w-h-100 socketresult" disabled></div></div><div class="d-flex"><div class="square square-lg mr-1"><button class="w-h-100 socketresult" disabled></div><div class="square square-lg"><button class="w-h-100 socketresult" disabled></div></div></div>');
     }
-
-
+    
     //change the current color when the user clicks on the color board
     $(".color").click(function () {
       let color = $(this).attr("id");
@@ -82,12 +97,12 @@ $(document).ready(function () {
     });
 
     //change the color of a board cell on click
-    $(".boardCell").click(function () {
+    $(".socket").click(function () {
       var id = $(this).attr("id");
 
-      if (isValid(id)) {
         $(this).css("background-color", currentColor);
-      }
+      
     });
+
   });
 });
