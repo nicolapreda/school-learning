d022aab956a6ba52ba1200227e29935dff3dc425
Nicola Preda <nicola.preda05@gmail.com>
2022-01-08 00:18:12 +0100

    Updated c++ exercises

---
+++ cpp/mcm_formula.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, resto, mcd, mcm;
+    cout << "Inserisci il primo numero:" << endl;
+    cin >> a;
+    do
+    {
+        cout << "Inserisci il secondo numero (deve essere maggiore del primo da te inserito): " << endl;
+        cin >> b;
+    } while (b <= a);
+
+    while (resto != 0)
+    {
+        resto = a % b;
+        if (resto == 0)
+        {
+            mcd = b;
+            break;
+        }
+        else
+        {
+            a = b;
+            b = resto;
+        }
+    }
+    cout<<"L'MCD dei due numeri è: "<<mcd<<endl;
+    cout<<b<<endl;
+    mcm = (a * b) / mcd;
+    cout << mcm;
+
+    return 0;
+}
