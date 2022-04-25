37ada672cc66644940153b17ed20c93262a7b963
Nicola Preda <nicola.preda05@gmail.com>
2022-04-25 10:48:42 +0200

    Updated linux exercises

--- linux/process.sh
+++ linux/process.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo ps -e | grep $1 > result
+#filter result with grep to take the first column
+ps -e | grep $1 | cut -b -7 > result
+
+#or use pidof function to automatically grab the pid
+pidof $1 > result
+
+# or filter result with awk to take the first column
+awk '{print $1}'ps -e > result
+
+
