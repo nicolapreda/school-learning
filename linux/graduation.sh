0a63de328627140e13c91e61c05653c04c3904b6
Nicola Preda <nicola.preda05@gmail.com>
2022-05-01 16:51:04 +0200

    Updated 'graduation.sh file'

--- linux/graduation.sh
+++ linux/graduation.sh
@@ -1,32 +1,59 @@
 #!/bin/bash
 
 # 1. get graduation of students in the file called as the first parameter
-cut -d ";" -f 4 $1 
+    cut -d ";" -f 4 $1 
 
 echo ""
 
 # 2. get graduation of students in the file called as the first parameter and in the subject called in second parameter
-grep $2 $1 | cut -d ";" -f 4 
+    grep $2 $1 | cut -d ";" -f 4 
 
 echo ""
 
 # 3. get the minimum graduation of the class in a subject
-grep $2 $1 | cut -d ";" -f 4 | sort -n | head -1
+    grep $2 $1 | cut -d ";" -f 4 | sort -n | head -1
 
 echo ""
 
 # 4. get the average of the class in a subject
-#n=5;
-#m=$n;
-#while [ $m -gt 0 ]; do
-#    num=${`grep $2 $1 | cut -d ";" -f 4``}
-#    sum=`expr $sum + $num`
-#
-#    n=`expr $n - 1`
-#done
+    count=0;
+    total=0; 
 
-#avg=`echo "$sum / $m" | bc -l`
-#echo $avg
+    for i in $( grep $2 $1 | cut -d ";" -f 4 )
+    do 
+        total=$(echo $total+$i | bc )
+        ((count++))
+    done
+    echo "scale=2; $total / $count" | bc
 
-# 5. 
-grep -w $2|$3 $1 | cut -d ";" -f 4 | sort -n | head -1
+echo ""
+
+
+# 5. get the minimum, maximum and average of the student in a subject
+    # parameter 1: file name parameter 2: subject name   parameter 3: student name, get minimum of the student in the subject
+    grep $2 $1 | grep $3 | cut -d ";" -f 4 | sort -n | head -1
+    # parameter 1: file name parameter 2: subject name   parameter 3: student name, get maximum of the student in that subject
+    grep $2 $1 | grep $3 | cut -d ";" -f 4 | sort -n | tail -1
+    # parameter 1: file name parameter 2: subject name   parameter 3: student name, get average graduation of the student in that subject
+    count=0;
+    total=0; 
+
+    for i in $( grep $2 $1 | grep $3 | cut -d ";" -f 4 )
+    do 
+        total=$(echo $total+$i | bc )
+        ((count++))
+    done
+    echo "scale=2; $total / $count" | bc
+
+echo ""
+
+# 6. get names of best students in that subject in that test
+    # parameter 1: file name parameter 2: subject name   parameter 3: date, get names of students that had more than 7 in that subject in that test
+    for i in $( grep $2 $1 | grep $3 )
+    do 
+        if [ $(echo $i | cut -d ";" -f 4) -gt 7 ]
+        then
+            echo $(echo $i | cut -d ";" -f 3)
+        fi
+    done
+    
