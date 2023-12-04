20bb1b8964c5cac6301d9b8cecb326daab60355f
Nicola Preda <nicola.preda05@gmail.com>
2023-12-04 12:46:20 +0100

    Updated API

--- node/api/index.js
+++ node/api/index.js
@@ -1,24 +1,78 @@
+/*
+CUSTOMERS
+
+CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone
+
+*/
+
 
 const express = require('express')
+const cors = require('cors');
+
+const yaml = require('yamljs')
+const swaggerUi = require('swagger-ui-express');
+const openApiValidator = require('express-openai-validator');
+
+
 const app = express()
-const port = 3000
 
-let categories = require("./db.json");
+app.use(express.json());
+app.use(cors("*")); // Abilita CORS
 
-app.get('/categories', async (req, res) => {
-        if(categories.length === 0){
-            return res.sendStatus(204); // Nessun contenuto
-        }
-        res.status(200).json(categories);
+const apiSpec = yaml.load("./api_spec")
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(apiSpec));
+
+const valitor = openApiValidator.middleware({
+  apiSpec: "api_specs.yaml",
+  validateRequest: true,
+  cali
+})
+
+let customers = require("./db.json");
+
+app.get('/customers', async (req, res) => {
+  if (customers.length === 0) {
+    return res.sendStatus(204); // Nessun contenuto
+  }
+  res.status(200).json(customers);
 
 })
 
-app.post('/categories', function (req, res) {
-    res.send('POST request to the homepage');
-  });
-  
+app.get('/customers/:customerId', async (req, res) => {
+  if (customers.length === 0) {
+    return res.sendStatus(204); // Nessun contenuto
+  }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  res.status(200).json(customers.params);
+
+})
+
+
+app.post('/customers',
+  async (req, res) => {
+
+    const newId = customers.reduce((acc, c) => (c.CompanyId > acc ? c.CompanyId : acc), 0) + 1;
+
+
+    //CustomerId, CompanyName, ContactName, ContactTitle, 
+    //Address, City, Region, PostalCode, Country, Phone
+
+    const newCustomer = {
+      customerId: newId,
+      companyName: req.body.companyName,
+      contactTitle: req.body.contactTitle,
+      address: req.body.address,
+    }
+
+    customers.push(newCustomer);
+    res.status(201).json(newCustomer);
+  }
+);
+
+app.delete('/customers/:customerId', )
+
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`)
 })
 
