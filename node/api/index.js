442bdc24e9944bdc0fdcc6c8dcc7a9a9577d6363
Nicola Preda <nicola.preda05@gmail.com>
2023-12-04 12:57:31 +0100

    Updated db

--- node/api/index.js
+++ node/api/index.js
@@ -11,7 +11,6 @@ const cors = require('cors');
 
 const yaml = require('yamljs')
 const swaggerUi = require('swagger-ui-express');
-const openApiValidator = require('express-openai-validator');
 
 
 const app = express()
@@ -19,14 +18,6 @@ const app = express()
 app.use(express.json());
 app.use(cors("*")); // Abilita CORS
 
-const apiSpec = yaml.load("./api_spec")
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(apiSpec));
-
-const valitor = openApiValidator.middleware({
-  apiSpec: "api_specs.yaml",
-  validateRequest: true,
-  cali
-})
 
 let customers = require("./db.json");
 
@@ -51,14 +42,13 @@ app.get('/customers/:customerId', async (req, res) => {
 app.post('/customers',
   async (req, res) => {
 
-    const newId = customers.reduce((acc, c) => (c.CompanyId > acc ? c.CompanyId : acc), 0) + 1;
 
 
     //CustomerId, CompanyName, ContactName, ContactTitle, 
     //Address, City, Region, PostalCode, Country, Phone
 
     const newCustomer = {
-      customerId: newId,
+      customerId: req.body.id,
       companyName: req.body.companyName,
       contactTitle: req.body.contactTitle,
       address: req.body.address,
