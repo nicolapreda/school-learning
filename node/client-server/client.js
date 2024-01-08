d32bf0aea63a90ed71e8e3502b7a552dc3599b8c
Nicola Preda <nicola.preda05@gmail.com>
2024-01-08 11:27:03 +0100

    Updated php folder

--- node/client-server/client.js
+++ node/client-server/client.js
@@ -1,48 +1,92 @@
 const dgram = require("dgram");
-const readlinePromise = require('readline-promise').default;
-
+const readlinePromise = require("readline-promise").default;
+const fs = require("fs");
+const { parse } = require("path");
 
 const rl = readlinePromise.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
-
-const client = dgram.createSocket("udp4");
 
 //Indirizzo o porta del server
 const serverAddress = "127.0.0.1";
 const serverPort = 12345;
+const clientPort = 54321;
 
-let username = '';
+const client = dgram.createSocket("udp4");
+client.bind(clientPort);
 
 const Main = async () => {
-    username = await rl.questionAsync("Inserisci il tuo nome utente: ")
-    client.send(username, serverPort, serverAddress);
-    while(true) {
-        const input = await rl.questionAsync("> ");
-        const message = `${username}: ${input}`;
-        client.send(message, serverPort, serverAddress);
-    }
-}
+  while (true) {
+    try {
+      const input = await rl.questionAsync(
+        "Scegli il servizio(0 => Elenco occorrenze, 1 => Testo senza spazi)"
+      );
+      let response, text;
 
+      switch (input) {
+        //primo servizio
+        case "0":
+          text = await rl.questionAsync("Inserisci il testo\n");
+          response = {
+            service: parseInt(input),
+            text: text,
+          };
+
+          break;
+        //secondo servizio
+        case "1":
+          text = await rl.questionAsync(
+            "Inserisci il percorso del file di testo\n"
+          );
+          aCapo = await rl.questionAsync(
+            "Vuoi anche eliminare il ritorno a capo? (y,n)\n"
+          );
+
+          if (aCapo === "y") {
+            aCapo = 1;
+          } else if (aCapo === "n") {
+            aCapo = 0;
+          } else {
+            aCapo = 0;
+          }
+          text = await fs.readFile(text, "utf8", (err, data) => {
+            if (err) throw err;
+            text = data;
+            return text;
+        });
+          console.log(text);
+
+          response = {
+            service: parseInt(input),
+            params: {
+              aCapo: aCapo,
+              text: text,
+            },
+          };
+          console.log(response);
+
+          break;
+        default:
+          break;
+      }
+
+      client.send(JSON.stringify(response), serverPort, serverAddress);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
 
 // Avvio del client
 Main()
-    .then(() => {
-        console.log("\nProgramma terminato");
-    }).catch((err) => {
-        console.error(`Errore: ${err}`)
-    });
-
-
+  .then(() => {
+    console.log("\nProgramma terminato");
+  })
+  .catch((err) => {
+    console.error(`Errore: ${err}`);
+  });
 
 client.on("message", (msg) => {
-    console.log(`>>>>>>>>>>>>>>>>> ${msg.toString()}`);
-    process.stdout.write('> ');
+  console.log(`Risposta del server: ${msg.toString()}\n`);
 });
-
-process.on("SIGINT", () => {
-    console.log("Chiusura del client")
-    client.close();
-    process.exit(0);
-})
--- node/client-server/server.js
+++ node/client-server/server.js
@@ -1,50 +1,116 @@
+
 const dgram = require("dgram");
 
 // Crea server UDP
 const server = dgram.createSocket("udp4");
 
-const clients = [];
+server.on("message", (req, reqInfo) => {
+    const portaMittente = reqInfo.port || 0;
+    const indirizzoMittente = reqInfo.address || "";
 
-server.on('message', (msg, info) => {
-    const data = JSON.parse(msg.toString());
-    const result = generateRandomNumber(data);
+  try {
+    // (1) mittente richiesta
+
+    if (portaMittente !== 54321 || indirizzoMittente !== "127.0.0.1") {
+      console.log("client non autorizzato");
+      return;
+    }
+
+    //(2) protocollo richiesta
+/*
+    if (!req.service || typeof req.service !== "number") {
+        throw new Error("Richiesta non valida");
+      }
+      if (!req.params.text || typeof req.text !== "string") {
+        throw new Error("Testo non valido");
+      }
+  */
+    const data = JSON.parse(req.toString());
+    console.log(data);
+
+
+
+    //(3) elaborazione risposta
+
+    let resultData = null;
+    let resultMex = null;
+
+    
+    switch(data.service){
+        case 0:
+            resultData = contaOccorrenze(data);
+            resultMex = "contaOccorrenze";
+            break;
+        case 1:
+            resultMex = "eliminaSpazi";
+            resultData = eliminaSpazi(data);
+            break;
+
+    }
+
+    //(4) risposta
     const response = {
-        risultato: result
-    }
+      status: "ok",
+      resultMex: resultMex,
+      risultato: resultData,
+    };
 
-    const message = msg.toString();
-    console.log(`Messaggio ricevuto dal client ${info.address}:${info.port}: ${message}`);
 
-    // Aggiunge il client alla lista se non è già presente
-    if(!clients.some(client => client.address === info.address && client.port === info.port)) {
-        clients.push(info);
-    }
+    const jsonRisposta = JSON.stringify(response, null, 2);
+    server.send(jsonRisposta, portaMittente, indirizzoMittente);
+  } catch (error) {
+    const response = {
+      status: "error",
+      statusMex: error.message,
+    };
 
-    //Inoltra il messaggio a tutti i client tranne a chi lo ha inviato
-    clients.forEach(clientInfo => {
-        if(clientInfo.port !== info.port || clientInfo.address !== info.address){
-            server.send(msg, clientInfo.port, clientInfo.address)
+    const jsonRisposta = JSON.stringify(response, null, 2);
+    server.send(jsonRisposta, portaMittente, indirizzoMittente);
+  }
+});
+
+
+
+const contaOccorrenze = (data) => {
+    const {service, text} = data;
+    var occorrenze = {};
+
+    // for per ogni carattere nel testo
+    for (var i = 0; i < text.length; i++) {
+        var carattere = text.charAt(i);
+
+        // Se carattere già presente, incrementa contatore
+        if (occorrenze[carattere]) {
+            occorrenze[carattere]++;
+        } else {
+            // sennò aggiungi a contatore iniziale 1
+            occorrenze[carattere] = 1;
         }
-    })
-})
+    }
+    return occorrenze;
+};
+
+const eliminaSpazi = (data) => {
+
+    console.log(data.params.text, parseInt(data.params.aCapo));
+    //Rimuovo le sequenze di spazi di lunghezza maggiore di 1
+    let stringaDef = text.replace(/\s{2,}/g, " ");
+
+    //controllo se l'utente vuole anche rimuovere gli "a capo"
+    if(aCapo == 1){
+        stringaDef = stringaDef.replace("\n", "");
+    }
+
+    return stringaDef;
+}
+
 
 // Connessione avviata
 server.on("listening", () => {
-    const address = server.address();
-    console.log(`Server UDP in ascolto su ${address.address}:${address.port}`);
-})
+  const address = server.address();
+  console.log(`Server UDP in ascolto su ${address.address}:${address.port}`);
+});
 
-
-const generateRandomNumber = (data) => {
-    const {mumero1, numero2} = data;
-
-    const min = Math.min(numero1, numero2);
-    const max = Math.min(numero1, numero2);
-
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-
-
-}
 
 
 //Inizio ad ascoltare sulla port 12345
